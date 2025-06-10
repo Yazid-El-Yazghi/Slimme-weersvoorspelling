5c11a11adad3e2a719164a26497e154dc4e4f1ab
Yazid-El-Yazghi <yaya250@live.fr>
2025-06-10 11:13:50 +0200

    Create project

---
+++ src/main/java/be/ehb/slimmeweervoospelling/SlimmeWeervoospellingApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.slimmeweervoospelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimmeWeervoospellingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlimmeWeervoospellingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/slimmeweervoospelling/SlimmeWeervoospellingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.slimmeweervoospelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlimmeWeervoospellingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

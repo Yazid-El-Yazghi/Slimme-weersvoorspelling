32541590b6e3fc96e466ce1010aa77a00aaf3138
Elion05 <elion.rexborn@gmail.com>
2025-06-11 13:06:54 +0200

    controllers aangemaakt

--- src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
+++ src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
@@ -1,4 +1,4 @@
-package be.ehb.slimmeweervoorspelling;
+package be.ehb.slimmeweervoospelling.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller

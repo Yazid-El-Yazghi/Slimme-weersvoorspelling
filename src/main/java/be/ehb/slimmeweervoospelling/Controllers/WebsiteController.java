c27226ce2eab2dbabcd4ef3a735b393c7fdbd674
Elion05 <elion.rexborn@gmail.com>
2025-06-11 11:58:55 +0200

    verder gewerkt aan de html themleaf

--- src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
+++ src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
@@ -1,31 +1,24 @@
 package be.ehb.slimmeweervoorspelling;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class WebsiteController {
-
     @GetMapping("/")
     public String home() {
         return "index"; // laadt index.html vanuit templates
     }
-
     @GetMapping("/admin")
     public String adminDashboard() {
         return "AdminIndex"; // laadt AdminIndex.html
     }
-
     @GetMapping("/alerts")
     public String alertsBeheer() {
         return "AlertsBeheer";
     }
-
     @GetMapping("/users")
     public String userBeheer() {
         return "AccountBeheer";
     }
-
     @GetMapping("/account")
     public String account() {
         return "Account";
@@ -34,5 +27,9 @@ public class WebsiteController {
     @GetMapping("/new-account")
     public String newAccount() {
         return "NieuwAccount";
+    }
+    @GetMapping("/weer")
+    public String voorspelling() {
+        return "weer";
     }
 }

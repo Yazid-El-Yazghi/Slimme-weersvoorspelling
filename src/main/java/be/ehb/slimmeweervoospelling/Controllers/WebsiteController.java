e914d5bd07a70a8b9755622a268098322d418da6
Elion05 <elion.rexborn@gmail.com>
2025-06-11 10:34:09 +0200

    controllers aangemaakt

---
+++ src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
@@ -0,0 +1,38 @@
+package be.ehb.slimmeweervoorspelling;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebsiteController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // laadt index.html vanuit templates
+    }
+
+    @GetMapping("/admin")
+    public String adminDashboard() {
+        return "AdminIndex"; // laadt AdminIndex.html
+    }
+
+    @GetMapping("/alerts")
+    public String alertsBeheer() {
+        return "AlertsBeheer";
+    }
+
+    @GetMapping("/users")
+    public String userBeheer() {
+        return "AccountBeheer";
+    }
+
+    @GetMapping("/account")
+    public String account() {
+        return "Account";
+    }
+
+    @GetMapping("/new-account")
+    public String newAccount() {
+        return "NieuwAccount";
+    }
+}

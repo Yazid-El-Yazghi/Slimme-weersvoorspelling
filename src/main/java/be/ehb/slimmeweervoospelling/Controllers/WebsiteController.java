3b60a75e7f87e590e807a01c278cf7992792c2d5
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2025-06-12 12:42:00 +0200

    bestanden verwijderen en bestanden code verwijderen en toevoegen

--- src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
+++ src/main/java/be/ehb/slimmeweervoospelling/Controllers/WebsiteController.java
@@ -9,31 +9,6 @@ public class WebsiteController {
         return "weer"; // toont de voorspellingen
     }
 
-    @GetMapping("/admin")
-    public String adminDashboard() {
-        return "Admin/AdminIndex"; // laadt Admin/AdminIndex.html
-    }
-
-    @GetMapping("/alerts")
-    public String alertsBeheer() {
-        return "Admin/AlertsBeheer"; // laadt Admin/AlertsBeheer.html
-    }
-
-    @GetMapping("/users")
-    public String userBeheer() {
-        return "Admin/AccountBeheer"; // laadt Admin/AccountBeheer.html
-    }
-
-    @GetMapping("/account")
-    public String account() {
-        return "Account"; // laadt Account.html
-    }
-
-    @GetMapping("/new-account")
-    public String newAccount() {
-        return "Admin/NieuwAccount"; // laadt Admin/NieuwAccount.html
-    }
-
     @GetMapping("/index")
     public String index() {
         return "index"; // laadt index.html
